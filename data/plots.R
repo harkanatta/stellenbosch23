86816ae449469450b713c8e7d709149fba99c568
Valtýr Sigurðsson <valtyr@biopol.is>
2023-06-29 21:46:20 +0000

    slæður og myndir

--- data/plots.R
+++ data/plots.R
@@ -38,26 +38,26 @@ print(p)
 ggsave("images/DominantGroups.png",width = 1280, height = 720, units = "px", dpi = 200, bg = "white")
 
 
-
-
 library(tidyverse)
 library(plyr)
 
-explore_column <- function(column_name) {
+explore_column <- function(column_name, years) {
   
+  data <- read_csv("data/merged_data_alltB.csv")
   # Check if column_name exists in the dataset
-  if (!column_name %in% names(merged_data_alltB)) {
+  if (!column_name %in% names(data)) {
     stop("Column does not exist in the dataset")
   }
   
   column_name <- as.character(column_name)
   
   # Read the data
-  data <- read_csv("data/merged_data_alltB.csv") %>%
+  data <- data %>%
     mutate(Group = case_when(
       Class == "Polychaeta" ~ "Polychaeta",
       Phylum == "Mollusca" ~ "Mollusca",
-      TRUE ~ "Everything else"))
+      TRUE ~ "Everything else")) %>%
+    filter(Artal %in% years)  # Filter data for specific years
   
   # Reverse the order of the Group factor
   data$Group <- factor(data$Group, levels = c("Everything else", "Mollusca", "Polychaeta"))
@@ -65,17 +65,28 @@ explore_column <- function(column_name) {
   # Order the levels of the year factor
   data$Artal <- factor(data$Artal, levels = c(2013:2017, 1999))
   
-  data <- data %>%
+  data <- data <- data[data$Group=="Polychaeta",] %>%
     ddply(c("Artal", column_name), summarise, Nfm = sum(Nfm)) %>%
-    arrange(desc(Nfm)) %>%
-    slice_head(n = 8)
+    arrange(Artal, desc(Nfm)) %>%
+    group_by(Artal) %>%
+    slice_head(n = 3) %>%
+    ungroup()
   
+  # Replace specific values in the specified column
+  value_to_replace <- c("Polydora", "Spionidae")
+  replacement_values <- c("Polydora spp.", "Spionidae spp.")
+  for(i in 1:length(value_to_replace)){
+    data[[column_name]] <- gsub(pattern = value_to_replace[i],
+                                          replacement = replacement_values[i],
+                                          x = data[[column_name]])
+  }
+
   # Create a bar plot
   p <- ggplot(data, aes_string(x = "Artal", y = "Nfm", fill = column_name)) +
     geom_bar(stat = "identity", position = "stack", color = "black") +
     scale_x_discrete(labels = function(x) ifelse(x == "1999", "1999", x)) +
-    scale_fill_brewer(palette = "Dark2") +
-    labs(x = NULL, y = "ind./m²", fill = "", title = " Benthic Macrofauna in Kolgrafafjörður") +
+    scale_fill_brewer(palette = "Paired", direction = -1) +
+    labs(x = NULL, y = "ind./m²", fill = "", title = "Top 3 Polychaetes each year") +
     theme_minimal() +
     theme(plot.title = element_text(size = 14, face = "bold", hjust = 0.5),
           axis.title = element_text(size = 12, face = "bold"),
@@ -87,10 +98,27 @@ explore_column <- function(column_name) {
           legend.direction = "horizontal",
           legend.title = element_blank())
   
-  # Print the plot
+
+  
   print(p)
+  return(str(data))
+  return(head(data))
 }
 
 # Usage
- explore_column("gamalt")
+explore_column("Flokkun", c(2013:2017,1999))
 
+ggsave("images/DominantPolychaetes.png",width = 1280, height = 720, units = "px", dpi = 100, bg = "white")
+
+
+
+
+
+
+
+for (i in c(2013:2017,1999)) {
+  print(length(unique(KolgrTaxa$Flokkun[KolgrTaxa$Artal==i])))
+}
+for (i in c(2013:2017,1999)) {
+  print(sum(data$N[data$Artal==i]))
+}
